7774283db8a2ec4a28d022ddcdef3862d248460e
shivamt0602 <106347133+shivamt0602@users.noreply.github.com>
2023-09-13 15:23:54 +0530

    Add files via upload
    used Link instead of a tag and to instead of href

--- Navbar.js
+++ Navbar.js
@@ -1,4 +1,5 @@
 import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
 
@@ -22,9 +23,38 @@ export default function Navbar(props) {
 
   return (
     <>
+    <div className='main-container'>
+    <nav className= "navbar navbar-expand-lg bg-dark color-white">
+  <div className="container-fluid">
+    <a className="navbar-brand text-white" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active text-white" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link text-white" to="/about">About</Link>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+      <div className="form-check form-switch">
+  <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked/>
+  <label className="form-check-label" for="flexSwitchCheckChecked">change state</label>
+  </div>
+    </div>
+  </div>
+</nav>
+</div>
+
     <div>
 <div className="mb-3">
-  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+  {/* <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label> */}
   <textarea className="form-control" id="exampleFormControlTextarea1" rows="10" value={text} onChange = {changing_func}></textarea>
 </div>
     </div>
